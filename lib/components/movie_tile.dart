71edb77ce68484c3a72efa7ff5ce83f43b753770
VladaBoholii <94959759+VladaBoholii@users.noreply.github.com>
2024-03-11 15:44:08 +0200

    title remove

--- lib/components/movie_tile.dart
+++ lib/components/movie_tile.dart
@@ -1,12 +1,11 @@
 import 'package:cinema_movies_app/model/movie.dart';
 import 'package:cinema_movies_app/pages/movie_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MovieTile extends StatefulWidget {
   final Movie movie;
 
-  const MovieTile({Key? key, required this.movie}) : super(key: key);
+  const MovieTile({super.key, required this.movie});
 
   @override
   State<MovieTile> createState() => _MovieTileState();
@@ -34,27 +33,6 @@ class _MovieTileState extends State<MovieTile> {
                 image: DecorationImage(
                   image: NetworkImage(widget.movie.posterPath),
                   fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                constraints: const BoxConstraints(maxWidth: 130),
-                decoration: BoxDecoration(
-                  borderRadius:
-                      const BorderRadius.only(topRight: Radius.circular(8)),
-                  color: Colors.purple.shade100,
-                ),
-                padding: const EdgeInsets.all(8),
-                child: Text(
-                  widget.movie.title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.purple[600],
-                  ),
-                  textAlign: TextAlign.center,
                 ),
               ),
             ),
